948962cc5ffecd98154c853c0811e01db31fe8d4
PritamGoyal <136835905+PritamGoyal@users.noreply.github.com>
2025-03-23 17:55:27 +0530

    app.py

--- app.py
+++ app.py
@@ -19,7 +19,13 @@ nltk.download('omw-1.4')
 nltk.download('punkt')
 # Uncomment the next line if you haven't downloaded the spacy model yet:
 # download('en_core_web_sm')
-nlp = spacy.load('en_core_web_sm')
+try:
+    nlp = spacy.load('en_core_web_sm')
+except OSError:
+    import subprocess
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"], check=True)
+    nlp = spacy.load('en_core_web_sm')
+
 
 # Load models and tokenizers
 bart_model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
